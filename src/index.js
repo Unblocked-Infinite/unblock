89eb8d08fa90a82eb90638cf6b498ddf03f75383
stellerNet <stellernet.org@gmail.com>
2023-08-04 09:37:15 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,84 @@
+import { createBareServer } from "@tomphttp/bare-server-node";
+import express from "express";
+import { createServer } from "node:http";
+import { publicPath } from "ultraviolet-static";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import compression from 'compression';
+
+const bare = createBareServer("/bare/");
+const app = express();
+
+const compressionOptions = {
+  filter: (req, res) => {
+    // Exclude sw.js file in /uv from compression
+    if (req.url === '/uv/sw.js') {
+      return false;
+    }
+    return true;
+  }
+};
+
+app.use(compression(compressionOptions));
+
+const staticOptions = {
+  maxAge: '1y',
+};
+
+app.use(express.static(publicPath, staticOptions));
+app.use("/uv/", express.static(uvPath, staticOptions));
+
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  bare.close();
+  process.exit(0);
+}
+
+server.listen({
+  port,
+});
